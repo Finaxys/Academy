fff16dd183539d2f1ba977fe21be184b4e99135b
Ines NEFOUSSI <inefoussi@finaxys.com>
2017-03-09 17:24:17 +0100

    Code refactoring
    Join tribute reward feature done

--- src/main/java/com/finaxys/slackbot/Configuration/Classes/SpringContext.java
+++ src/main/java/com/finaxys/slackbot/Configuration/Classes/SpringContext.java
@@ -1,14 +1,10 @@
 package com.finaxys.slackbot.Configuration.Classes;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
-import com.finaxys.slackbot.BUL.Classes.NewTributeJoinedServiceImpl;
-import com.finaxys.slackbot.BUL.Interfaces.NewTributeJoinedService;
-import com.finaxys.slackbot.DAL.Classes.Repository;
-import com.finaxys.slackbot.Domains.FinaxysProfile;
 import com.finaxys.slackbot.BUL.Listeners.MessageListener;
+import com.finaxys.slackbot.DAL.Classes.Repository;
+import com.finaxys.slackbot.Domains.Challenge;
+import com.finaxys.slackbot.Domains.FinaxysProfile;
+import com.finaxys.slackbot.Utilities.Classes.MessageAppreciatedListner;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,10 +17,8 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.finaxys.slackbot.DAL.Classes.Repository;
-import com.finaxys.slackbot.Domains.Challenge;
-import com.finaxys.slackbot.Domains.FinaxysProfile;
-import com.finaxys.slackbot.Utilities.Classes.MessageAppreciatedListner;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by inesnefoussi on 3/6/17.
@@ -35,58 +29,67 @@ import com.finaxys.slackbot.Utilities.Classes.MessageAppreciatedListner;
 @PropertySource(value = "classpath:dataSourceInformation.properties")
 public class SpringContext {
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	private Properties hibernateProperties() {
-		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-		return properties;
-	}
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-		return dataSource;
-	}
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return dataSource;
+    }
 
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource());
-		sessionFactory.setPackagesToScan(new String[] { "com.finaxys.slackbot.*" });
-		sessionFactory.setHibernateProperties(hibernateProperties());
-		return sessionFactory;
-	}
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[]{"com.finaxys.slackbot.*"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
 
-	@Bean
-	@Autowired
-	public HibernateTransactionManager transactionManager(SessionFactory s) {
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(s);
-		return txManager;
-	}
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
+    }
 
-	@Bean
-	public Repository<FinaxysProfile, String> myGenericRepo1() {
-		return new Repository<FinaxysProfile, String>(FinaxysProfile.class);
-	}
+    @Bean
+    public Repository<FinaxysProfile, String> myGenericRepo1() {
+        return new Repository<FinaxysProfile, String>(FinaxysProfile.class);
+    }
 
-	@Bean
-	public Repository<Challenge, Integer> myGenericRepo2() {
-		return new Repository<Challenge, Integer>(Challenge.class);
-	}
+    @Bean
+    public Repository<Challenge, Integer> myGenericRepo2() {
+        return new Repository<Challenge, Integer>(Challenge.class);
+    }
 
-	@Bean
-	public MessageAppreciatedListner messageAppreciatedListner() {
-		return new MessageAppreciatedListner();
-	}
+    @Bean
+    public MessageAppreciatedListner messageAppreciatedListner() {
+        return new MessageAppreciatedListner();
+    }
 
+    @Bean
+    public Repository<FinaxysProfile, String> finaxysProfileManager() {
+        return new Repository<>(FinaxysProfile.class);
+    }
+
+    @Bean
+    public MessageListener listener() {
+        return new MessageListener();
+    }
 }
