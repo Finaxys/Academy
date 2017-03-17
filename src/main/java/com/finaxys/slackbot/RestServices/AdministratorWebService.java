bd06e7f3f5fb72b221630ff8982c71fad7008dba
Mahdi Ben Amar <mbenamar@finaxys.com>
2017-03-17 09:29:42 +0100

    Display All challenge managers implemented
    Changes:
    - RestServices/AdministratorWebService.java

--- src/main/java/com/finaxys/slackbot/RestServices/AdministratorWebService.java
+++ src/main/java/com/finaxys/slackbot/RestServices/AdministratorWebService.java
@@ -3,10 +3,10 @@ package com.finaxys.slackbot.RestServices;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finaxys.slackbot.DAL.Repository;
-import com.finaxys.slackbot.Domains.Challenge;
 import com.finaxys.slackbot.Domains.FinaxysProfile;
 import com.finaxys.slackbot.Domains.Message;
 import com.finaxys.slackbot.Utilities.PropertyLoader;
+import com.finaxys.slackbot.Utilities.SlackBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,7 +36,39 @@ public class AdministratorWebService {
                                                                         @RequestParam("user_id") String adminFinaxysProfileId,
                                                                         @RequestParam("text") String arguments) {
         if (propertiesAreNotEqual("verification_token", token)) {
-            Message message = new Message("Wrong verification token !"+propertyLoader.loadSlackBotProperties().getProperty("verification_token"));
+            Message message = new Message("Wrong verification token !" + propertyLoader.loadSlackBotProperties().getProperty("verification_token"));
+            return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
+        }
+        ;
+
+        if (propertiesAreNotEqual("finaxys_team_name", teamId)) {
+            Message message = new Message("Only for FinaxysTM members !");
+            return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
+        }
+        ;
+
+        if (userIsNotAdministrator(adminFinaxysProfileId)) {
+            Message message = new Message("You don't have administration authorization !");
+            return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
+        }
+        ;
+
+        String finaxysProfileId = splitTextToArguments(arguments).get(0);
+        FinaxysProfile finaxysProfile = finaxysProfileRepository.findById(finaxysProfileId);
+        finaxysProfile = (finaxysProfile == null) ? new FinaxysProfile() : finaxysProfile;
+        finaxysProfile.setChallengeManager(true);
+        finaxysProfileRepository.saveOrUpdate(finaxysProfile);
+        Message message = new Message(finaxysProfile.getName() + "has just became a challenge manager!");
+        return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/challenge_manager/", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<JsonNode> getChallengeManagers(@RequestParam("token") String token,
+                                                         @RequestParam("team_domain") String teamId,
+                                                         @RequestParam("user_id") String adminFinaxysProfileId) {
+        if (propertiesAreNotEqual("verification_token", token)) {
+            Message message = new Message("Wrong verification token !" + propertyLoader.loadSlackBotProperties().getProperty("verification_token"));
             return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
         };
 
@@ -50,29 +82,28 @@ public class AdministratorWebService {
             return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
         };
 
-        String finaxysProfileId = splitTextToArguments(arguments).get(0);
-        FinaxysProfile finaxysProfile = finaxysProfileRepository.findById(finaxysProfileId);
-        finaxysProfile = (finaxysProfile == null) ? new FinaxysProfile() : finaxysProfile;
-        finaxysProfile.setChallengeManager(true);
-        finaxysProfileRepository.saveOrUpdate(finaxysProfile);
-        Message message = new Message(finaxysProfile.getName() + "has just became a challenge manager!");
+        List<FinaxysProfile> finaxysProfiles = finaxysProfileRepository.getByCriterion("challenge_manager",true);
+        String messageText = "Challenge managers' list:\n";
+        for(FinaxysProfile finaxysProfile:finaxysProfiles)
+            messageText += "- " + SlackBot.getSlackWebApiClient().getUserInfo(finaxysProfile.getId()).getName() + " \n";
+        Message message = new Message(messageText);
         return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
     }
 
-    public boolean propertiesAreNotEqual(String propertyName, String propertyValue){
+    public boolean propertiesAreNotEqual(String propertyName, String propertyValue) {
         return !propertyValue.equals(propertyLoader.loadSlackBotProperties().getProperty(propertyName));
     }
 
-    public boolean userIsNotAdministrator(String adminFinaxysProfileId){
+    public boolean userIsNotAdministrator(String adminFinaxysProfileId) {
         FinaxysProfile adminFinaxysProfile = finaxysProfileRepository.findById(adminFinaxysProfileId);
         return !adminFinaxysProfile.isAdministrator();
     }
 
-    public List<String> splitTextToArguments(String text){
+    public List<String> splitTextToArguments(String text) {
         List<String> arguments = new ArrayList<>();
-        while ((text.indexOf("<") != -1) && (text.indexOf("<") < text.indexOf(">"))){
-            arguments.add(text.substring(text.indexOf("@")+1, text.indexOf("|")));
-            text = text.substring(text.indexOf(">")+1);
+        while ((text.indexOf("<") != -1) && (text.indexOf("<") < text.indexOf(">"))) {
+            arguments.add(text.substring(text.indexOf("@") + 1, text.indexOf("|")));
+            text = text.substring(text.indexOf(">") + 1);
         }
         return arguments;
     }
