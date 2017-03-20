2f6a44d363dc8e7b251d02bca4e971c20ed9644c
ChoukSahar <schouk@finaxys.com>
2017-03-20 11:57:58 +0100

    djiudg

--- src/main/java/com/finaxys/slackbot/RestServices/EventApi.java
+++ src/main/java/com/finaxys/slackbot/RestServices/EventApi.java
@@ -26,27 +26,25 @@ public class EventApiInitializer {
 
     @RequestMapping(value = "/eventApi", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> initializeEventApi(@RequestParam("challenge") String challenge,
-                                                     @RequestParam("token") String token,
-                                                     @RequestParam("team_id") String teamId,
-                                                     @RequestParam("team_id") JsonNode event) {
-        if (!challenge.isEmpty())
-            return new ResponseEntity(challenge, HttpStatus.OK);
+    public ResponseEntity<String> initializeEventApi(@RequestBody JsonNode jsonNode) {
+        if (jsonNode.has("challenge"))
+            return new ResponseEntity(jsonNode.get("challenge").asText(), HttpStatus.OK);
 
-        if (propertiesAreNotEqual("verification_token", token)) {
+        if (propertiesAreNotEqual("verification_token", jsonNode.get("token").asText())) {
             Message message = new Message("Wrong verification token !");
             return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
         }
 
-        if (propertiesAreNotEqual("finaxys_team_id", teamId)) {
+       if (propertiesAreNotEqual("team_id", jsonNode.get("team_id").asText())) {
             Message message = new Message("Only for FinaxysTM members !");
             return new ResponseEntity(objectMapper.convertValue(message, JsonNode.class), HttpStatus.OK);
         }
 
-        String eventType = event.get("type").asText();
-
+        String eventType = jsonNode.get("type").asText();
+        System.out.println("event Type = " +eventType);
+        System.out.println("event = " +jsonNode.toString());
         if (eventType.equals("message.channels"))
-            realMessageReward.rewardReadlMessage(event.get("event"));
+            realMessageReward.rewardReadlMessage(jsonNode.get("event"));
 
         return new ResponseEntity(HttpStatus.OK);
     }
