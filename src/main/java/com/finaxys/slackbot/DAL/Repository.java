98c0189547f348c3ef4c82a6832bfaab732b1d77
arnaudbaboulin <ababoulin@finaxys.com>
2017-04-13 11:21:34 +0200

    repo refacto

--- src/main/java/com/finaxys/slackbot/DAL/Repository.java
+++ src/main/java/com/finaxys/slackbot/DAL/Repository.java
@@ -66,6 +66,85 @@ public class Repository<T, K extends Serializable> {
     public void saveOrUpdate(T entity) {
         sessionFactory.getCurrentSession().saveOrUpdate(entity);
     }
+    
+    public List<T> getByCriterion(Object ...objects)
+    {
+    	Criteria criteria = sessionFactory.getCurrentSession().createCriteria(persistentClass);
+    	
+    	for (int i = 0; i < objects.length - 1; i+=2)
+    		criteria.add(Restrictions.eq(objects[i].toString(), objects[i+1]));
+    		
+    	return criteria.list();
+    }
+}
+package com.finaxys.slackbot.DAL;
+
+import com.finaxys.slackbot.Utilities.Timer;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.List;
+
+@org.springframework.stereotype.Repository
+@Transactional
+public class Repository<T, K extends Serializable> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    private Class persistentClass;
+
+    public Repository() {
+    }
+
+    public Repository(Class<T> persistentClass) {
+        this.persistentClass = persistentClass;
+    }
+
+    public T addEntity(T entity) {
+        return (T) sessionFactory.getCurrentSession().save(entity);
+    }
+
+    public void updateEntity(T entity) {
+        sessionFactory.getCurrentSession().update(entity);
+
+    }
+
+    public void delete(T entity) {
+        sessionFactory.getCurrentSession().delete(entity);
+    }
+
+    public List<T> getAll() {
+        return sessionFactory.getCurrentSession()
+                .createQuery("from " + persistentClass.getSimpleName())
+                .list();
+    }
+
+    public List<T> getAllOrderedByAsList(String orderedByField, boolean ascending, int rowsCount) throws IllegalArgumentException {
+        // args check
+        if (orderedByField == null || orderedByField.isEmpty())
+            throw new IllegalArgumentException("orderedByField must not be null or empty.");
+        if (rowsCount < 1)
+            throw new IllegalArgumentException("rowsCount=" + rowsCount + ". Must be > 0.");
+        // work
+        List<T> x = sessionFactory.getCurrentSession()
+                .createQuery("from " + persistentClass.getSimpleName()
+                        + " f ORDER BY f." + orderedByField + (ascending ? "" : " DESC"))
+                .setMaxResults(rowsCount)
+                .list();
+        return x;
+    }
+
+    public T findById(K id) {
+        return (T) sessionFactory.getCurrentSession().get(persistentClass, id);
+    }
+
+    public void saveOrUpdate(T entity) {
+        sessionFactory.getCurrentSession().saveOrUpdate(entity);
+    }
 
     public List<T> getByCriterion(String criterion, Object criterionValue) {
         Criteria criteria = sessionFactory.getCurrentSession().createCriteria(persistentClass);
