2e662aef3110ac9783a63d59e77e0f72428f7f57
Kacel Nacim <nkacel@finaxys.com>
2017-04-14 15:45:31 +0200

    Refactoring BUL.Interfaces

--- src/main/java/com/finaxys/slackbot/BUL/Interfaces/ChannelLeftService.java
+++ src/main/java/com/finaxys/slackbot/BUL/Interfaces/ChannelLeftService.java
@@ -2,9 +2,7 @@ package com.finaxys.slackbot.BUL.Interfaces;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-/**
- * Created by inesnefoussi on 3/13/17.
- */
 public interface ChannelLeftService {
+	
     void onChannelLeaveMessage(JsonNode jsonNode);
 }
--- src/main/java/com/finaxys/slackbot/BUL/Interfaces/NewTribeJoinedService.java
+++ src/main/java/com/finaxys/slackbot/BUL/Interfaces/NewTribeJoinedService.java
@@ -2,9 +2,7 @@ package com.finaxys.slackbot.BUL.Interfaces;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-/**
- * Created by inesnefoussi on 3/7/17.
- */
 public interface NewTribeJoinedService {
+	
     void onNewTribeJoined(JsonNode jsonNode);
 }
