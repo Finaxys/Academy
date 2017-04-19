983f10438421a8ee9998b59fe692cb48c6020475
Julien REMY <jremy@finaxys.com>
2017-04-19 12:14:14 +0200

    debug

--- src/main/java/com/finaxys/slackbot/WebServices/AdministratorWebService.java
+++ src/main/java/com/finaxys/slackbot/WebServices/AdministratorWebService.java
@@ -58,7 +58,9 @@ public class AdministratorWebService extends BaseWebService {
         if (!isAdmin(userId)) 
         {
         	SlackUser finaxysProfile = finaxysProfileRepository.findById(userId);
-        	
+        	System.out.println(finaxysProfile);
+        	System.out.println(userId);
+        	System.out.println(userName);
             finaxysProfile = (finaxysProfile == null) ? new SlackUser(userId, userName) : finaxysProfile;
             
             finaxysProfileRepository.saveOrUpdate(finaxysProfile);
