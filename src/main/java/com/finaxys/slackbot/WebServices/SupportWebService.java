3007dae9a0dbeacd323eb6dcca566f14bb41c9da
finaxys <akhaldi@finaxys.com>
2018-03-07 14:38:31 +0100

    test_sirine_09

--- src/main/java/com/finaxys/slackbot/WebServices/SupportWebService.java
+++ src/main/java/com/finaxys/slackbot/WebServices/SupportWebService.java
@@ -48,7 +48,7 @@ public class SupportWebService extends BaseWebService {
                          "*/fxadmin_list_params* \n List all parameters \n \n";
         timer.capture();
         
-        String message = "/fx_help\nList of the FX334443 bot commands:\n " + fxCommands + (isAdmin(userId) ? " \n " + fxAdminCommands : "");
+        String message = "/fx_help\nList of the FX33443 bot commands:\n " + fxCommands + (isAdmin(userId) ? " \n " + fxAdminCommands : "");
 
         timer.capture();
         return newResponseEntity(message +timer,true);
