020e4c576d13c0fa0d5891e5b0af31240520ee81
Atef Bouhlel <abouhlel@finaxys.com>
2018-05-04 16:50:00 +0200

    fx-help works

--- src/main/java/com/finaxys/slackbot/BUL/Listeners/MessageListener.java
+++ src/main/java/com/finaxys/slackbot/BUL/Listeners/MessageListener.java
@@ -9,10 +9,10 @@ import com.finaxys.slackbot.BUL.Interfaces.InnovateService;
 import com.finaxys.slackbot.BUL.Interfaces.NewTribeJoinedService;
 import com.finaxys.slackbot.BUL.Interfaces.RealMessageReward;
 import com.finaxys.slackbot.Utilities.SlackBot;
+import com.finaxys.slackbot.interfaces.HelpService;
 
 @Component
-public class MessageListener implements EventListener 
-{
+public class MessageListener implements EventListener {
 
 	@Autowired
 	private NewTribeJoinedService newTribeJoinedService;
@@ -25,35 +25,78 @@ public class MessageListener implements EventListener
 
 	@Autowired
 	private ChannelLeftService channelLeftService;
-
-	public MessageListener() {}
 	
-	private void analyseMessage(JsonNode jsonNode) {
-		String message = jsonNode.get("text").asText();
-		SlackBot.postMessageToDebugChannelAsync("Hi " + SlackBot.getSlackWebApiClient().getUserInfo(jsonNode.get("user").asText()).getName());
-		
+	@Autowired
+	private HelpService helpService ;
+
+	public MessageListener() {
 	}
 
-	public void handleMessage(JsonNode jsonNode) 
-	{
+	private void analyseMessage(JsonNode jsonNode) {
+		String message = jsonNode.get("text").asText().trim();
+		String[] command = message.split(" ");
+		switch (command[0]) {
+		case "fx_help":
+			if(command.length == 1) {
+				SlackBot.postMessageToDebugChannelAsync(helpService.fx_help());
+			}
+			else
+			{
+				SlackBot.postMessageToDebugChannelAsync("fx_help ne prend pas d'arguments");
+				
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private static String getHelpCommands() {
+		String fxCommands = "*/fx_event_list* \n List all the events. \n \n"
+				+ "*/fx_event_add* [event name] [group|individual] [description] \n Adds a new event. \n \n"
+				+ "*/fx_event_named* [event name] \n Gives an event details. \n \n"
+				+ "*/fx_events_by_date* [yyyy-mm-dd] \n List of events by date \n \n"
+				+ "*/fx_events_by_type* [group|individual] \n List of events by type \n \n"
+				+ "*/fx_event_score_list*  [eventName] event \n Gives the score list of a given event. \n \n"
+				+ "*/fx_event_del* \n Removes an event! \n \n"
+				+ "*/fx_event_score_add* @username [points] [name] \n Adds a score to an event attendee. \n \n"
+				+ "*/fx_action_add* \n [Code] [ActionName] [Points] \n \n"
+				+ "*/fx_action_score_add* \n [EventName] [UserName] [ActionCode] \n \n"
+				+ "*/fx_manager_add* [event name] @username \n Adds an event manager. \n \n"
+				+ "*/fx_manager_list* [event name] \n List of event managers . \n \n"
+				+ "*/fx_manager_del* eventName @username \n Removes an event manager. \n \n"
+				+ "*/fx_leaderboard* [optional: count] \n Gives the top scores. \n \n"
+				+ "*/fx_contest_add* [contest] [points earned] \n Adds a w<contest. \n \n"
+				+ "*/fx_score* [userName] \n Show a user's scores \n"
+				+ "*/fxadmin_list* \n List1 of all administrators. \n \n";
+
+		String fxAdminCommands = "*/fxadmin_add* @username \n Adds an administrator. \n \n"
+				+ "*/fxadmin_del* @username \n Removes an administrator. \n \n"
+				+ "*/fxadmin_param* [parameter_name] [parameter_value] \n Set the value of a parameter \n \n"
+				+ "*/fxadmin_list_params* \n List all parameters \n \n";
+
+		return "/fx_help\nList of the FX bot commands:\n" + fxCommands;
+
+	}
+
+	public void handleMessage(JsonNode jsonNode) {
 		if (!jsonNode.has("subtype")) {
 			realMessageReward.rewardReadMessage(jsonNode);
-			
+
 			System.out.println(jsonNode.get("text").asText());
-			
+
 			analyseMessage(jsonNode);
-			
-		}else 
-		{
-			System.out.println("coucoucou");
+
+		} else {
 			String messageSubtype = jsonNode.get("subtype").asText();
 
 			if (messageSubtype.equals("channel_join"))
 				newTribeJoinedService.onNewTribeJoined(jsonNode);
-			
+
 			else if (messageSubtype.equals("file_share"))
 				innovateService.rewardFileShared(jsonNode);
-			
+
 			else if (messageSubtype.equals("channel_leave"))
 				channelLeftService.onChannelLeaveMessage(jsonNode);
 		}
---
+++ src/main/java/com/finaxys/slackbot/interfaces/HelpService.java
@@ -0,0 +1,6 @@
+package com.finaxys.slackbot.interfaces;
+
+public interface HelpService {
+
+	public String fx_help();
+}
