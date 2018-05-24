1fbba3631b63c07ee6f268dfc91beed858fa1778
Unknown <ymahmoudi@finaxys.com>
2018-05-24 10:38:19 +0200

    small change fx_help

--- src/main/java/com/finaxys/slackbot/services/HelpServiceImpl.java
+++ src/main/java/com/finaxys/slackbot/services/HelpServiceImpl.java
@@ -19,7 +19,7 @@ public class HelpServiceImpl implements HelpService {
                 		 "*fx_events_list* \n List all the events. \n \n" +
                          "*fx_event_add* [event name] [description] [group or individual]  \n Adds a new event. \n \n" +
                          "*fx_event_details* [event name] \n Gives an event details. \n \n" +
-                         "*fx_events_by_date* [yyyy-mm-dd] \n List of events by date \n \n" +
+                         "*fx_events_by_date* [yyyy-mm-dd] \n List of events by specified date \n \n" +
                          "*fx_events_by_type* [group or individual] \n List of events by type \n \n" +
                          "*fx_action_add* [event name] [action name] [description] [points] \n Adds a new action \n \n" +
                          "*fx_action_performed* [event name] [action name] [user name] \n Adds an action to the user specified \n \n" +
@@ -28,14 +28,12 @@ public class HelpServiceImpl implements HelpService {
         
 
         String fxAdminCommands =
-        				 "*fxadmin_add* @username \n Adds an administrator. \n \n" +
-                         "*fxadmin_del* @username \n Removes an administrator. \n \n" +
-                         "*fxadmin_param* [parameter_name] [parameter_value] \n Set the value of a parameter \n \n"+
-                         "*fxadmin_list_params* \n List all parameters \n \n" +
-                         "*fxadmin_list* \n List1 of all administrators. \n \n";
+        				 "*fxadmin_add* [user name or password] \n Adds an administrator if you have the permission or giving the right password promote you to an administrator. \n \n" +
+        				 "*fxadmin_enable_debug* \n Activate the debug mode for testing. \n \n" +
+        				 "*fxadmin_disable_debug* \n Desactivate the debug mode. \n \n";
         
         
-        String message = "/fx_help\nList of the FX bot commands:\n" + fxCommands ;
+        String message = "List of the FX bot commands:\n" + fxCommands ;
 
         timer.capture();
         return message + timer;
