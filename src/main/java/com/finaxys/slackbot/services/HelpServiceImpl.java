ccff00e8e70a7c138dd22a39bb189cc282810ffd
Unknown <ymahmoudi@finaxys.com>
2018-05-29 11:29:52 +0200

    improve helpservice messages

--- src/main/java/com/finaxys/slackbot/services/HelpServiceImpl.java
+++ src/main/java/com/finaxys/slackbot/services/HelpServiceImpl.java
@@ -10,23 +10,25 @@ public class HelpServiceImpl implements HelpService {
     	
         String fxCommands =
                 		 "*fx_events_list* \n List all the events. \n \n" +
-                         "*fx_event_add* eventName description group|individual  \n Adds a new event. \n \n" +
+                         "*fx_event_add* eventName description group|individual \n Adds a new event. \n \n" +
+                         "*fx_event_remove* eventName \n Removes an event. \n \n" +
                          "*fx_event_details* eventName \n Gives an event details. \n \n" +
-                         "*fx_events_by_date* [yyyy-mm-dd] \n List of events by specified date \n \n" +
-                         "*fx_events_by_type* [group or individual] \n List of events by type \n \n" +
-                         "*fx_action_add* [event name] [action name] [description] [points] \n Adds a new action \n \n" +
-                         "*fx_action_performed* [event name] [action name] [user name] \n Adds an action to the user specified \n \n" +
-                         "*fx_manager_add* username eventName \n add a manager to an event. \n \n" +
-                         "*fx_manager_remove* username eventName \n remove a manager from an event. \n \n" +
+                         "*fx_events_by_date* yyyy-mm-dd \n List of events by specified date. \n \n" +
+                         "*fx_events_by_type* group|individual \n List of events by type. \n \n" +
+                         "*fx_action_add* eventName actionName description points \n Adds a new action. \n \n" +
+                         "*fx_action_remove* eventName actionName \n Removes the specified action. \n \n" +
+                         "*fx_action_performed* eventName actionName userName \n Adds an action to the user specified \n \n" +
+                         "*fx_manager_add* userName eventName \n Adds a manager to an event. \n \n" +
+                         "*fx_manager_remove* userName eventName \n Removes a manager from an event. \n \n" +
                          "*fx_leaderboard* [eventName|limitNumber] \n Gives the scores. \n \n" +
-                         "*fx_score* username \n Show a user's scores \n";
+                         "*fx_score* userName \n Shows a user's scores \n";
         
 
         String fxAdminCommands =
-        				 "*fxadmin_add* username | password \n Adds an administrator if you have the permission or giving the right password promote you to an administrator. \n \n" +
-        				 "*fxadmin_remove* username password \n remove an administrator if you have the permission to remove an administrator. \n \n" +
-        				 "*fxadmin_enable_debug* \n Activate the debug mode for testing. \n \n" +
-        				 "*fxadmin_disable_debug* \n Desactivate the debug mode. \n \n";
+        				 "*fxadmin_add* userName|password \n Adds an administrator if you have the permission or giving the right password promote you to an administrator. \n \n" +
+        				 "*fxadmin_remove* userName password \n Removes an administrator if you have the permission to remove an administrator. \n \n" +
+        				 "*fxadmin_enable_debug* \n Activates the debug mode for testing. \n \n" +
+        				 "*fxadmin_disable_debug* \n Desactivates the debug mode. \n \n";
         
         
         String message = "List of the FX bot commands:\n" + fxCommands + (isAdmin ? " \n " + fxAdminCommands : "");;
