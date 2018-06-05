a7322afaaed22d3d913c5cdde079d19a0c376d37
Atef Bouhlel <abouhlel@finaxys.com>
2018-06-05 14:52:22 +0200

    fix date

--- src/main/java/com/finaxys/slackbot/DAL/Event.java
+++ src/main/java/com/finaxys/slackbot/DAL/Event.java
@@ -1,6 +1,8 @@
 package com.finaxys.slackbot.DAL;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,34 +21,33 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.apache.commons.lang3.time.DateUtils;
 
 @Entity
 @Table(name = "EVENT")
 public class Event implements Serializable {
 
-    
 	private static final long serialVersionUID = 1L;
 	private Integer eventId;
-    private String 	name;
-    private String 	description;
-    @Temporal(TemporalType.DATE)
-    private Date 	creationDate;
-    private String 	type;
-    //private Set<SlackUserEvent> attendees;
-    public Set<Action> eventScores;
-    
-    
-    private Set<SlackUser> eventManagers;
+	private String name;
+	private String description;
+	@Temporal(TemporalType.DATE)
+	private Date creationDate;
+	private String type;
+	// private Set<SlackUserEvent> attendees;
+	public Set<Action> eventScores;
 
-    public Event() {
-        creationDate = new Date();
-        //attendees = new HashSet<>();
-        eventManagers = new HashSet<>();
-    }    
+	private Set<SlackUser> eventManagers;
 
-    public Event(String name, String description, String type) {
+	public Event() {
+		creationDate = new Date();
+		// attendees = new HashSet<>();
+		eventManagers = new HashSet<>();
+	}
+
+	public Event(String name, String description, String type) {
 		super();
-		//attendees = new HashSet<>();
+		// attendees = new HashSet<>();
 		this.name = name;
 		this.description = description;
 		this.creationDate = new Date();
@@ -55,97 +56,100 @@ public class Event implements Serializable {
 	}
 
 	@Id
-    @Column(name = "EVENT_ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Integer getEventId() {
-        return eventId;
-    }
+	@Column(name = "EVENT_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Integer getEventId() {
+		return eventId;
+	}
 
-    public void setEventId(Integer id) {
-        this.eventId = id;
-    }
+	public void setEventId(Integer id) {
+		this.eventId = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public Date getCreationDate() {
-        return creationDate;
-    }
+	public Date getCreationDate() {
+		return creationDate;
+	}
 
-    public void setCreationDate(Date creationDate) { this.creationDate = creationDate; }
-    public String getType() {
-        return type;
-    }
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    /*
-    @JsonIgnore
-    @OneToMany(mappedBy = "event", cascade = CascadeType.REMOVE , fetch = FetchType.EAGER)
-    public Set<SlackUserEvent> getAttendees() {
-        return attendees;
-    }
-    
-    public void setAttendees(Set<SlackUserEvent> attendees) {
-        this.attendees = attendees;
-    }
-    */
-    
-    @OneToMany(mappedBy = "event", fetch = FetchType.EAGER)
-    public Set<Action> getEventScores() {
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/*
+	 * @JsonIgnore
+	 * 
+	 * @OneToMany(mappedBy = "event", cascade = CascadeType.REMOVE , fetch =
+	 * FetchType.EAGER) public Set<SlackUserEvent> getAttendees() { return
+	 * attendees; }
+	 * 
+	 * public void setAttendees(Set<SlackUserEvent> attendees) { this.attendees =
+	 * attendees; }
+	 */
+
+	@OneToMany(mappedBy = "event", fetch = FetchType.EAGER)
+	public Set<Action> getEventScores() {
 		return eventScores;
 	}
 
 	public void setEventScores(Set<Action> eventScores) {
 		this.eventScores = eventScores;
 	}
-	
+
 	private String displayActions() {
 		StringBuilder sb = new StringBuilder();
-		this.eventScores.forEach(x->sb.append("\n\t* "+ x.getCode() + " : " + x.getDescription() +"\t("+ x.getPoints() +" points)\n"));
+		this.eventScores.forEach(x -> sb
+				.append("\n\t* " + x.getCode() + " : " + x.getDescription() + "\t(" + x.getPoints() + " points)\n"));
 		return sb.toString();
 	}
-	
+
 	private String displayEventManagers() {
 		StringBuilder sb = new StringBuilder();
-		//this.eventManagers.forEach(m->sb.append("\t* "+ SlackBot.getSlackWebApiClient().getUserInfo(m.getSlackUserId()).getProfile().getReal_name() +"\n"));
-		this.eventManagers.forEach(m->sb.append("\t* <@"+ m.getSlackUserId() +">\n"));
+		// this.eventManagers.forEach(m->sb.append("\t* "+
+		// SlackBot.getSlackWebApiClient().getUserInfo(m.getSlackUserId()).getProfile().getReal_name()
+		// +"\n"));
+		this.eventManagers.forEach(m -> sb.append("\t* <@" + m.getSlackUserId() + ">\n"));
 		return sb.toString();
 	}
 
 	public String toString() {
-    	return "Event name: " + this.getName() + "\n"
-				+ "Description: " + this.getDescription() + "\n"
-				+ "Type: " + this.getType() + "\n"
-			    + "Creation date : " + this.getCreationDate() + "\n"
-				//+ "Number of participants: " + this.getAttendees().size() + " \n "
-			    + "Event's managers : \n" + this.displayEventManagers() + "\n" 
-    			+ "Event's actions "+ (this.eventScores.isEmpty() ? "(0)" : ":") + this.displayActions();
-    } 
-    
-    public boolean equals (Event event) {
-    	return event.getEventId()==this.getEventId();
-    }
-//, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST}
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "SLACK_USER_EVENT",
-            inverseJoinColumns = @JoinColumn(name = "SLACK_USER_ID", nullable = false, updatable = false),
-            joinColumns = @JoinColumn(name = "EVENT_ID", nullable = false, updatable = false))
+		return "Event name: " + this.getName() + "\n" + "Description: " + this.getDescription() + "\n" + "Type: "
+				+ this.getType() + "\n" + "Creation date : " + this.getCreationDate() + "\n"
+				// + "Number of participants: " + this.getAttendees().size() + " \n "
+				+ "Event's managers : \n" + this.displayEventManagers() + "\n" + "Event's actions "
+				+ (this.eventScores.isEmpty() ? "(0)" : ":") + this.displayActions();
+	}
+
+	public boolean equals(Event event) {
+		return event.getEventId() == this.getEventId();
+	}
+
+	// , cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH,
+	// CascadeType.PERSIST}
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "SLACK_USER_EVENT", inverseJoinColumns = @JoinColumn(name = "SLACK_USER_ID", nullable = false, updatable = false), joinColumns = @JoinColumn(name = "EVENT_ID", nullable = false, updatable = false))
 	public Set<SlackUser> getEventManagers() {
 		return eventManagers;
 	}
@@ -153,6 +157,5 @@ public class Event implements Serializable {
 	public void setEventManagers(Set<SlackUser> eventManagers) {
 		this.eventManagers = eventManagers;
 	}
-    
-    
+
 }
